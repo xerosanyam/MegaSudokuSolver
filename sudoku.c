ce640aea129cbcda000f1a99c3d32cb9deeafa73
Sanyam Jain <xerosanyam@gmail.com>
2014-11-24 00:28:42 +0530

    Learnings:trying to make dynamic 3 dimension array

--- sudoku.c
+++ sudoku.c
@@ -1,27 +1,33 @@
 /*
- * Version 3 of Sudoku
- * Learnings:trying to make dynamic 2 dimension array
+ * Version 4 of Sudoku
+ * Learnings:trying to make dynamic 3 dimension array
  * 
 */
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-	int i=0,j=0;
-	int *a[3];
-//	*a=(int*)malloc(sizeof(int)*5);
-	for(i=0;i<3;i++){
-		a[i]=(int*)malloc(sizeof(int)*3);
-	}
-	for(i=0;i<3;i++){
-		for(j=0;j<3;j++){
-			scanf("%d",&a[i][j]);
+	int i=0,j=0,k=0;
+	int *a[9][9];
+	for(i=0;i<9;i++){
+		for(j=0;j<9;j++){
+		a[i][j]=(int*)malloc(sizeof(int)*9);
 		}
 	}
-	for(i=0;i<3;i++){
-		for(j=0;j<3;j++){
-			printf("%d ",a[i][j]);
+	for(i=0;i<9;i++){
+		for(j=0;j<9;j++){
+			for(k=0;k<9;k++){
+				scanf("%d",&a[i][j][k]);
+			}
 		}
-		printf("\n");
+	}
+	for(i=0;i<9;i++){
+		printf("For i = %d\n",i);
+		for(j=0;j<9;j++){
+			for(k=0;k<9;k++){
+				printf("%d ",a[i][j][k]);
+			}
+			printf("\n");
+		}
 	}
 	return 0;
 }
