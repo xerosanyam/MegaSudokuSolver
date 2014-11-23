7378a073239d778ae62cb0a684ac7e04a25adda6
Sanyam Jain <xerosanyam@gmail.com>
2014-11-24 00:33:20 +0530

    *size of array kept 10;changed input format to separated by space

--- sudoku.c
+++ sudoku.c
@@ -1,30 +1,32 @@
 /*Latest
- * Version 6 of Sudoku
- * Learnings:breaking into functions
+ * Version 7 of Sudoku
+ * Change Log: 
+ * 1. size of array kept 10 : increases time by 0.7 ms
+ * 2. changed input format to separated by space
  * 
 */
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#define size 3
+#define psize 10
 void createSudoku();
-void fillSudoku();
-void printSudoku();
-void printHintRow();
-void fillHintRow();
+void fillSudoku();					//using input
+void printSudoku();					//print values in sudoku (used in start and end)
+void printHintRow();				//prints hint row
+void fillHintRow();					//1 in hint list means, that number cannot come.
 void fillHintRowUsingSudkoRow();
 void fillHintRowUsingSudkoColumn();
-void fillHintRowUsingSudkoCell();
-void fillNumUsingHintRow();
-int i=0,j=0,k=0,len=0;
-int *a[size*size][size*size];
+void fillHintRowUsingSudkoCell();	
+void fillNumUsingHintRow();			//Rule 1
+int i=0,j=0,k=0,len=0,size=0;
+int *a[psize*psize][psize*psize];
 int main(){
 	clock_t start = clock();
 	createSudoku();			//Create Data Structure.
 	fillSudoku();			//Fill input values
 	printSudoku();
-	fillHintRow();
-	fillNumUsingHintRow();
+	fillHintRow();			//Initial filling of hint row
+	fillNumUsingHintRow();	//Rule 1
 	printSudoku();
 	clock_t stop = clock();
     double elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
@@ -33,6 +35,8 @@ int main(){
 }
 void createSudoku(){
 	int i,j;
+	scanf("%d",&size);
+	getchar();
 	len=size*size;
 	for(i=0;i<len;i++){
 		for(j=0;j<len;j++){
@@ -41,12 +45,21 @@ void createSudoku(){
 	}
 }
 void fillSudoku(){
-	int i,j;
+	int i,j,ch,num=0;
 	for(i=0;i<len;i++){
-			for(j=0;j<len;j++){
-				scanf("%d",&a[i][j][0]);
+		for(j=0;j<len;j++){
+			num=0;
+			while(1){
+				ch=getchar();\
+				if(ch==' ' || ch=='\n'){
+					break;
+				}
+				else{
+					num=num*10+(ch-'0');
+				}
 			}
-			
+			a[i][j][0]=num;									//Very important step; fills value in sudoku.
+		}
 	}
 }
 void printSudoku(){
@@ -81,11 +94,11 @@ void fillHintRowUsingSudkoRow(){
 	int i,j,col=0,num=0;
 	for(i=0;i<len;i++){
 		for(j=0;j<len;j++){
-			if(a[i][j][0]>0){
-				num=a[i][j][0];
+			if(a[i][j][0]>0){								//if a number is filled in sudoku
+				num=a[i][j][0];								//get that number in num
 				for(col=0;col<len;col++){
-					if(a[i][col][0]==0)a[i][col][num]=1;
-				}
+					if(a[i][col][0]==0)a[i][col][num]=1;	//for its row, where there is a blank, update its hint list that it num cannot come
+				}											//1 in hint list means, that number cannot come
 			}
 		}
 	}
@@ -95,10 +108,10 @@ void fillHintRowUsingSudkoColumn(){
 	int i,j,row=0,num=0;
 	for(i=0;i<len;i++){
 		for(j=0;j<len;j++){
-			if(a[i][j][0]>0){
-				num=a[i][j][0];
+			if(a[i][j][0]>0){								//if a number is filled in sudoku
+				num=a[i][j][0];								//get that number in num
 				for(row=0;row<len;row++){
-					if(a[row][j][0]==0)a[row][j][num]=1;
+					if(a[row][j][0]==0)a[row][j][num]=1;	//place 1 in all blank cell in all rows corresponding to that number	
 				}
 			}
 		}
@@ -148,7 +161,7 @@ void fillNumUsingHintRow(){						//Fills number using hint row only. Rule 1
 				}
 				if(sum==(len-1)){
 					a[i][j][0]=num;
-					printf("%d Number found in %d row, %d col\n",num,i,j);
+					printf("%d Filled in %d row, %d col\n",num,i+1,j+1);
 					i=-1;
 					j=-1;
 					fillHintRow();
