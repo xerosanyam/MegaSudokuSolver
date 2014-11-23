28d26aa078a7deba0151c716e769dbd77c72d990
Sanyam Jain <xerosanyam@gmail.com>
2014-11-24 00:26:30 +0530

    Learnings:trying to make dynamic 2 dimension array

--- sudoku.c
+++ sudoku.c
@@ -1,19 +1,27 @@
 /*
- * Version 2 of Sudoku
- * learnings:trying to make dynamic 1 dimension array
+ * Version 3 of Sudoku
+ * Learnings:trying to make dynamic 2 dimension array
  * 
 */
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-	int i=0;
-	int *a;
-	a=(int*)malloc(sizeof(int)*5);
-	for(i=0;i<5;i++){
-		scanf("%d",&a[i]);
+	int i=0,j=0;
+	int *a[3];
+//	*a=(int*)malloc(sizeof(int)*5);
+	for(i=0;i<3;i++){
+		a[i]=(int*)malloc(sizeof(int)*3);
 	}
-	for(i=0;i<5;i++){
-		printf("%d",a[i]);
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
 	}
 	return 0;
 }
