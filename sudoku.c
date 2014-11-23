cd53f54ee5d5179b9af8b1ff70aab72e48ec43c2
Sanyam Jain <xerosanyam@gmail.com>
2014-11-24 00:23:43 +0530

    Learnings:trying to make dynamic 1 dimension array

--- sudoku.c
+++ sudoku.c
@@ -1,18 +1,19 @@
 /*
- * Version 1 of Sudoku
- * learnings: n dimension matrices are allowed in C
+ * Version 2 of Sudoku
+ * learnings:trying to make dynamic 1 dimension array
  * 
 */
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-	int i;
-	int a[5][5][5][5][5][5];
+	int i=0;
+	int *a;
+	a=(int*)malloc(sizeof(int)*5);
 	for(i=0;i<5;i++){
-	scanf("%d",&a[i][i][i][i][i][i]);
+		scanf("%d",&a[i]);
 	}
 	for(i=0;i<5;i++){
-	printf("%d",a[i][i][i][i][i][i]);
+		printf("%d",a[i]);
 	}
 	return 0;
 }
